5daef90f5935b7f7b6dea639cdd6582985f25bc8
Matthew Gilligan <mcgilligan97@gmail.com>
2021-01-30 00:20:36 +0000

    Render project details in template

--- src/templates/ProjectDetails/index.js
+++ src/templates/ProjectDetails/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-import MainLayout from './../../layouts/MainLayout'
+import MainLayout from './../../layouts/MainLayout';
 
-const ProjectDetails = () => {
+export const query = graphql`
+  query($slug: String!) {
+    contentfulProject(slug: { eq: $slug }) {
+      title
+      creationDate(formatString: "MMMM Do, YYYY")
+    }
+  }
+`
+
+const ProjectDetails = props => {
     return (
         <MainLayout>
-            <h1>Project</h1>
+            <h1>{props.data.contentfulProject.title}</h1>
+            <p>{props.data.contentfulProject.creationDate}</p>
         </MainLayout>
     )
 };
