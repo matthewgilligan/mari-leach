417772446837ec797cdbc083ee9688383adedd40
Matthew Gilligan <mcgilligan97@gmail.com>
2021-01-30 16:02:20 +0000

    Add richtext render to aboutinfo component

--- src/components/AboutInfo/index.js
+++ src/components/AboutInfo/index.js
@@ -2,19 +2,32 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import './styles.scss';
+import RichText from './../RichText';
 
 const AboutInfo = () => {
     const data = useStaticQuery(graphql`
         query {
             contentfulSiteMetaData {
                 title
+                bio {
+                    raw
+                }
             }
         }
     `)
 
+    const { title, bio } = data.contentfulSiteMetaData;
+
+    const json = JSON.parse(bio.raw)
+
+    const configRichText = {
+        json: json,
+    };
+
     return (
         <div>
-            <h1>{data.contentfulSiteMetaData.title}</h1>
+            <h1>{title}</h1>
+            <RichText {...configRichText} />
         </div>
     )
 };
