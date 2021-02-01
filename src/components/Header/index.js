056389e8fde4a8f546147537bec2fd7b0015ccdf
Matthew Gilligan <mcgilligan97@gmail.com>
2021-02-01 17:59:36 +0000

    Add email contact to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 import './styles.scss';
 
 const Header = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            contentfulSiteMetaData {
+                email
+            }
+        }
+    `)
+
+    const { email } = data.contentfulSiteMetaData;
+
     return (
         <header>
             <Link to="/">Mari Leach</Link>
-            <Link to="/about">About</Link>
+            <div className="info">
+                <Link to="/about">About</Link>
+                <a href={`mailto:${email}`} target="_blank" rel="noreferrer">Contact</a>
+            </div>
         </header>
     )
 };
