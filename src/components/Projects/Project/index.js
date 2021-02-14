2a7b2d18fdd78c1122126a574445c9bb89067c0c
Matthew Gilligan <mcgilligan97@gmail.com>
2021-02-14 20:17:42 +0000

    Add title on hover

--- src/components/Projects/Project/index.js
+++ src/components/Projects/Project/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Project = (project) => {
-    const { slug, images } = project;
+    const { slug, images, title } = project;
 
     return (
         <div className="project">
-            <Link to={`/projects/${slug}`} className="thumbnail">
+            <Link to={`/projects/${slug}`} >
                 <img src={images[0].file.url} alt={images.title} className="image"/>
+                <div className="overlay">
+                    <h1>{title}</h1>
+                </div>
             </Link>
         </div>
     )
